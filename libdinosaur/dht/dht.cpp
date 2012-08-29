adb66e7a26ebaf0aff8e3b0817ace375100ae2d4
RuslanKutdusov <ruslan.kutdusov.usu@gmail.com>
2012-08-30 01:48:27 +0600

    well, let`s begin!

--- libdinosaur/dht/dht.cpp
+++ libdinosaur/dht/dht.cpp
@@ -12,6 +12,89 @@ namespace dinosaur
 namespace dht
 {
 
+dht::dht(network::NetworkManager* nm, const node_id & our_id)
+{
+	m_nm = nm;
+	m_node_id = our_id;
+}
+
+dht::~dht()
+{
+
+}
+
+void dht::InitSocket(const in_addr & listen_on, uint16_t port)
+{
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	memcpy(&addr.sin_addr, &listen_on, sizeof(in_addr));
+	addr.sin_port = htons(port);
+	m_sock_status.listen = false;
+	m_sock_status.errno_ = 0;
+	m_sock_status.exception_errcode = Exception::NO_ERROR;
+	try
+	{
+		m_nm->Socket_add_UDP(shared_from_this(), m_sock, &addr);
+	}
+	catch(Exception & e)
+	{
+		m_sock_status.exception_errcode = e.get_errcode();
+		return;
+	}
+	catch(SyscallException & e)
+	{
+		m_sock_status.errno_ = e.get_errno();
+		return;
+	}
+	m_sock_status.listen = true;
+}
+
+const socket_status & dht::get_socket_status()
+{
+	return m_sock_status;
+}
+
+void dht::prepare2release()
+{
+	m_nm->Socket_delete(m_sock);
+}
+
+int dht::event_sock_ready2read(network::Socket sock)
+{
+
+}
+
+int dht::event_sock_closed(network::Socket sock)
+{
+
+}
+
+int dht::event_sock_sended(network::Socket sock)
+{
+
+}
+
+int dht::event_sock_connected(network::Socket sock)
+{
+
+}
+
+int dht::event_sock_accepted(network::Socket sock, network::Socket accepted_sock)
+{
+
+}
+
+int dht::event_sock_timeout(network::Socket sock)
+{
+
+}
+
+int dht::event_sock_unresolved(network::Socket sock)
+{
+
+}
+
+
 }
 }
 
--- libdinosaur/dht/dht.h
+++ libdinosaur/dht/dht.h
@@ -10,7 +10,10 @@
 
 #include "../consts.h"
 #include "../types.h"
+#include "../network/network.h"
 #include <map>
+#include <set>
+#include <boost/shared_ptr.hpp>
 
 namespace dinosaur
 {
@@ -24,12 +27,69 @@ private:
 	void clear();
 public:
 	node_id();
+	node_id(const node_id & id);
+	node_id(const char * id);
+	virtual ~node_id();
+	node_id & operator=(const node_id & id);
+	node_id & operator=(const char * id);
 	unsigned char & operator[](size_t i);
 	const unsigned char & operator[](size_t i) const;
-	virtual ~node_id();
+	const node_id operator ^(const node_id & id);
+	bool operator<(const node_id & id);
+	bool operator==(const node_id & id);
+	bool operator!=(const node_id & id);
+	void print();
 };
 
-node_id distance(const node_id & n1, const node_id & n2);
+node_id distance(const node_id & id1, const node_id & id2);
+node_id generate_random_node_id();
+void generate_random_node_id(node_id & id);
+size_t get_bucket(const node_id & id1, const node_id & id2);
+
+class dht;
+typedef boost::shared_ptr<dht> dhtPtr;
+
+class dht : public network::SocketAssociation
+{
+private:
+	enum REQUEST
+	{
+		REQUEST_PING,
+		REQUEST_FIND_NODE,
+		REQUEST_GET_PEERS,
+		REQUEST_ANNOUNCE_PEER
+	};
+	std::map<node_id, std::set<uint16_t> > 	m_requests;
+	node_id									m_node_id;
+	network::NetworkManager*				m_nm;
+	network::Socket							m_sock;
+	socket_status							m_sock_status;
+	dht(){}
+	dht(const dht & dht){}
+	dht & operator=(const dht & dht){ return *this;}
+	dht(network::NetworkManager* nm, const node_id & our_id);
+public:
+	static void CreateDHT(const in_addr & listen_on, uint16_t port, network::NetworkManager* nm, dhtPtr & ptr, const node_id & our_id = generate_random_node_id())
+	{
+		if (nm == NULL)
+			throw Exception(Exception::ERR_CODE_NULL_REF);
+		ptr.reset(new dht(nm, our_id));
+		if (ptr == NULL)
+			throw Exception(Exception::ERR_CODE_UNDEF);
+		ptr->InitSocket(listen_on, port);
+	}
+	virtual ~dht();
+	void InitSocket(const in_addr & listen_on, uint16_t port);
+	const socket_status & get_socket_status();
+	void prepare2release();
+	int event_sock_ready2read(network::Socket sock);
+	int event_sock_closed(network::Socket sock);
+	int event_sock_sended(network::Socket sock);
+	int event_sock_connected(network::Socket sock);
+	int event_sock_accepted(network::Socket sock, network::Socket accepted_sock);
+	int event_sock_timeout(network::Socket sock);
+	int event_sock_unresolved(network::Socket sock);
+};
 
 }
 }
